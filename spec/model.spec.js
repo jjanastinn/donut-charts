a20bea39fc73c7e134da15ea0fd27a06de510be1
Jana Stinn <jana-stinn@gmx.de>
2018-11-08 19:18:02 +0100

    second test working

--- spec/model.spec.js
+++ spec/model.spec.js
@@ -2,7 +2,7 @@ import Model from '../src/models/model';
 
 describe('Model', function() {
 
-  it('should be a function', function() {
+  it('should sum the absolute numbers', function() {
     const data = {
       devices: [
         {amount: 120},
@@ -11,6 +11,17 @@ describe('Model', function() {
     }
     const model = new Model(data);
     expect(model.calculateSum()).toEqual(200);
+  });
+
+  it('should calculate the percentages', function() {
+    const data = {
+      devices: [
+        {amount: 120},
+        {amount: 80}
+      ]
+    }
+    const model = new Model(data);
+    expect(model.calculatePercent(data.devices[1])).toEqual(40);
   });
 
 });
---
+++ spec/view.spec.js
@@ -0,0 +1,6 @@
+import View from '../src/views/view';
+import Model from '../src/models/model';
+
+describe('Model', function() {
+
+});
